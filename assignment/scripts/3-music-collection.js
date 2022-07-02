6718c2646a4a02f413fdb4fee4a099766ad64aa4
Jonathon Baxendell <j.bax626@gmail.com>
2022-07-02 11:01:25 -0500

    logged albums

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -15,15 +15,27 @@ function addToCollection (title, artist, yearPublished) {
 
 addToCollection('In Rainbows', 'Radiohead', 2007);
 
+console.log(collection[0]);
+
 addToCollection('King Of Limbs', 'Radiohead', 2011);
+
+console.log(collection[1]);
 
 addToCollection('Blonde', 'Frank Ocean', 2016);
 
+console.log(collection[2]);
+
 addToCollection('Humanz', 'Gorillaz', 2017);
+
+console.log(collection[3]);
 
 addToCollection('Damn.', 'Kendrick Lamar', 2017);
 
+console.log(collection[4]);
+
 addToCollection('The Doors', 'The Doors', 1967);
+
+console.log(collection[5]);
 
 console.log(collection);
 
